ab6e76b05e33dc019288073af3e7004b94b3e37f
christian <awelakoue@gmail.com>
2018-11-10 08:32:31 +0200

    Create User

--- public/index.php
+++ public/index.php
@@ -4,21 +4,91 @@
 
     require '../vendor/autoload.php';
 
-    require '../includes/DbConnect.php';
+    require '../includes/DbOperations.php';
 
     $app = new \Slim\App;
-    $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-        $name = $args['name'];
-        $response->getBody()->write("Hello, $name");
 
-        // Checking DB Connection
-        $db = new DbConnect();
-        
-        if($db->connect() != null)
-        {
-            echo 'Connection succesfull';
+    /**
+     *  Endpoint : createUser
+     *  Paramters: email, password, name, school
+     *  Method: POST
+     */
+
+    $app->post('/createuser', function(Request $request, Response $request){
+
+        if(!haveEmptyParameters(array('email', 'password', 'name', 'school'), $response)){
+            $request_data = $request->getParseBody();
+
+            $email = $request_data['email'];
+            $password = $request_data['password'];
+            $name = $request_data['name'];
+            $school = $request_data['school'];
+
+            // Encrypt password, DO NOT store password in plain text format
+            $hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Create DbOperations instance
+            $db = new DbOperations;
+
+            $result =$db->createUser($email, $hash_password, $name, $school);
+
+            // Chec the user status
+            if($result == USER_CREATED) {
+                $message = array();
+                $message['error'] = false;
+                $message['message'] = 'User created successfully';
+
+                $response->write(json_encode($message));
+
+                return $response
+                                ->withHeader('Content-type', 'application/json')
+                                ->withStatus(201);
+
+
+            } else if ($result == USER_FAILURE) {
+                $message = array();
+                $message['error'] = true;
+                $message['message'] = 'Some error occurred';
+
+                $response->write(json_encode($message));
+
+                return $response
+                                ->withHeader('Content-type', 'application/json')
+                                ->withStatus(422);
+
+            } else if ($result == USER_EXISTS) {
+                $message = array();
+                $message['error'] = true;
+                $message['message'] = 'User Already Existed';
+
+                $response->write(json_encode($message));
+
+                return $response
+                                ->withHeader('Content-type', 'application/json')
+                                ->withStatus(422);
+            }
+        }
+    });
+
+    function haveEmptyParameters($required_params, $response) {
+        $error = false;
+        $error_params = ''; 
+        $request_params = $_REQUEST;
+
+        foreach($request_params as $param) {
+            if(!isset($request_params[$param]) || strlen($request_params[$param]) <= 0) {
+                $error = true;
+                $error_params .= $param . ', ';
+            }
         }
 
-        return $response;
-    });
+        if($error){
+            $error_detail = array();
+            $error_detail['detail'] = true;
+            $error_details['message'] = 'Required paramaters' . substr($error_params, 0, -2) . ' are missing or empty';
+            $response->write(json_encode($error_detail));
+        }
+        return $error; 
+    }
+    // run the app
     $app->run();
