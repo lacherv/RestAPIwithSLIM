2119ecc7103d46fb58ea0c519484eef08b70c83d
christian <awelakoue@gmail.com>
2018-11-12 16:42:44 +0200

    User login

--- includes/DBOperations.php
+++ includes/DBOperations.php
@@ -33,6 +33,55 @@
             }
             return USER_EXISTS;
         }
+
+        
+        public function userLogin($email, $password)
+        {
+            if($this->isEmailExist($email))
+            {
+                $hashed_password = $this->getUsersPasswordByEmail($email); 
+                if(password_verify($password, $hashed_password))
+                {
+                    return USER_AUTHENTICATED;
+                } else {
+                    return USER_PASSWORD_DO_NOT_MATCH; 
+                }
+            } else {
+                return USER_NOT_FOUND;    
+            }
+        }
+
+        /*  
+            The method is returning the password of a given user
+            to verify the given password is correct or not
+        */
+        public function getUsersPasswordByEmail($email){
+            $stmt = $this->con->prepare("SELECT password FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute(); 
+            $stmt->bind_result($password);
+            $stmt->fetch(); 
+            return $password; 
+        }
+
+        /*
+            The Read Operation
+            This function reads a specified user from database
+        */
+        public function getUserByEmail($email){
+            $stmt = $this->con->prepare("SELECT id, email, name, school FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute(); 
+            $stmt->bind_result($id, $email, $name, $school);
+            $stmt->fetch(); 
+            $user = array(); 
+            $user['id'] = $id; 
+            $user['email']=$email; 
+            $user['name'] = $name; 
+            $user['school'] = $school; 
+            return $user; 
+        }
+        
         /*
             The Read Operation
             The function is checking if the user exist in the database or not
